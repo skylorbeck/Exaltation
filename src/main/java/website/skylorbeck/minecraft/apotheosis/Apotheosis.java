0dac2553b49ae1f5538bf265f870f27e03745002
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-08-20 16:40:39 -0700

    test classes, test leveling
    Signed-off-by: Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>

--- src/main/java/website/skylorbeck/minecraft/apotheosis/Apotheosis.java
+++ src/main/java/website/skylorbeck/minecraft/apotheosis/Apotheosis.java
@@ -1,10 +1,18 @@
 package website.skylorbeck.minecraft.apotheosis;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
 
 public class Apotheosis implements ModInitializer {
     @Override
     public void onInitialize() {
-
+/*        MinecraftClient.getInstance().getServer().getCommandManager().execute(new ServerCommandSource(server, new Vec3d(user.getX(), user.getY(), user.getZ()), Vec2f.ZERO, (ServerWorld) world, 4, "Chunk Loader", new LiteralText("Chunk Loader"), server, null),
+                String.format("forceload %s %s %s", "remove", blockPos.getX(), blockPos.getZ()));*/
     }
 }
--- src/main/java/website/skylorbeck/minecraft/apotheosis/client/ApotheosisClient.java
+++ src/main/java/website/skylorbeck/minecraft/apotheosis/client/ApotheosisClient.java
@@ -1,11 +1,37 @@
 package website.skylorbeck.minecraft.apotheosis.client;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.fabricmc.fabric.impl.client.keybinding.KeyBindingRegistryImpl;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.option.KeyBinding;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.LiteralText;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.lwjgl.glfw.GLFW;
 
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class ApotheosisClient implements ClientModInitializer{
+    public static KeyBinding testbind = KeyBindingHelper.registerKeyBinding(new KeyBinding("test",InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_Z,"test"));
     @Override
     public void onInitializeClient() {
-
+        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+            while (testbind.wasPressed()) {
+                MinecraftServer server = client.getServer();
+                assert server != null;
+                ServerWorld world = server.getOverworld();
+                PlayerEntity user = client.player;
+                assert user != null;
+                server.getCommandManager().execute(new ServerCommandSource(server, new Vec3d(user.getX(), user.getY(), user.getZ()), Vec2f.ZERO, world, 4, "Apotheosis", new LiteralText("Apotheosis"), server, null),
+                String.format("advancement grant "+client.player.getEntityName()+" only apotheosis:warsmith"));
+            }
+        });
     }
 }
