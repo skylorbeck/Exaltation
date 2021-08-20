4d3f50758d0dae711143df59d218fd0d37323f33
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-08-19 20:27:16 -0700

    Initial commit

---
+++ src/main/java/website/skylorbeck/minecraft/apotheosis/Apotheosis.java
@@ -0,0 +1,10 @@
+package website.skylorbeck.minecraft.apotheosis;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Apotheosis implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/website/skylorbeck/minecraft/apotheosis/client/ApotheosisClient.java
@@ -0,0 +1,11 @@
+package website.skylorbeck.minecraft.apotheosis.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
+public class ApotheosisClient implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
