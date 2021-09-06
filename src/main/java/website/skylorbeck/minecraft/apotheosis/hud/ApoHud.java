5234c72e398045e7f8a67d74924be1fc54ea36e8
Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>
2021-09-06 14:02:15 -0700

    marksman bow powers.
    Signed-off-by: Skylortrexler <42103122+Skylortrexler@users.noreply.github.com>

--- src/main/java/website/skylorbeck/minecraft/apotheosis/hud/ApoHud.java
+++ src/main/java/website/skylorbeck/minecraft/apotheosis/hud/ApoHud.java
@@ -1,10 +1,17 @@
 package website.skylorbeck.minecraft.apotheosis.hud;
 
+import io.github.apace100.apoli.component.PowerHolderComponent;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.potion.PotionUtil;
 import net.minecraft.util.DyeColor;
+import website.skylorbeck.minecraft.apotheosis.powers.MarksmanArrowCyclingPower;
 
 import static website.skylorbeck.minecraft.apotheosis.cardinal.ApotheosisComponents.APOXP;
 
@@ -15,15 +22,25 @@ public class ApoHud {
     private void render(MatrixStack matrixStack, float delta) {
         MinecraftClient client = MinecraftClient.getInstance();
         TextRenderer textRenderer = client.inGameHud.getTextRenderer();
+        ItemRenderer itemRenderer = client.getItemRenderer();
         int scaledWidth = client.getWindow().getScaledWidth();//these are CRITICAL for dynamically scaled ui elements
         int scaledHeight = client.getWindow().getScaledHeight();
         String string = String.valueOf(APOXP.get(client.player).getLevel());
         int x = (scaledWidth - textRenderer.getWidth(string)) / 2;
-        int y = scaledHeight - 31 - 14;
+        int y = scaledHeight - 45;
         textRenderer.draw(matrixStack, (String)string, (float)(x + 1), (float)y, 0);
         textRenderer.draw(matrixStack, (String)string, (float)(x - 1), (float)y, 0);
         textRenderer.draw(matrixStack, (String)string, (float)x, (float)(y + 1), 0);
         textRenderer.draw(matrixStack, (String)string, (float)x, (float)(y - 1), 0);
         textRenderer.draw(matrixStack, string, (float)x, (float)y, DyeColor.ORANGE.getSignColor());
+        PlayerEntity entity = MinecraftClient.getInstance().player;
+        if (PowerHolderComponent.hasPower(entity, MarksmanArrowCyclingPower.class)) {
+            MarksmanArrowCyclingPower marksmanArrowCyclingPower = PowerHolderComponent.KEY.get(entity).getPowers(MarksmanArrowCyclingPower.class).get(0);
+            if (marksmanArrowCyclingPower.isActive()) {
+                ItemStack newArrow = Items.TIPPED_ARROW.getDefaultStack();
+                PotionUtil.setPotion(newArrow, marksmanArrowCyclingPower.getPotion());
+                itemRenderer.renderGuiItemIcon(newArrow,scaledWidth-scaledWidth/4-16,scaledHeight-30);
+            }
+        }
     }
 }
